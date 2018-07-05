b50d88889a080b7fe48b51a0ce0cc79a9beec1fd
Alexandra Klevtsova <alexandra.klevtsova@dataart.com>
2018-07-05 17:51:34 +0300

    Fixed all issues

--- CSharpBasics/TaskModule6/Program.cs
+++ CSharpBasics/TaskModule6/Program.cs
@@ -16,33 +16,32 @@ namespace TaskModule6
         static void Main(string[] args)
         {
             //test
-            Temperature temp1 = new Temperature(15);
-            Temperature temp2 = new Temperature(-12.5);
-            Temperature temp3 = new Temperature(12m, isCelsius: false);
-            Temperature temp4 = new Temperature(48, isCelsius: false);
+            var temp1 = new Temperature(15);
+            var temp2 = new Temperature(-12.5m);
+            var temp3 = new Temperature(12m, isCelsius: false);
+            var temp4 = new Temperature(48, isCelsius: false);
 
             Console.WriteLine(temp2);
             Console.WriteLine(temp3);
-            Console.WriteLine(temp1 + temp2);
-            Console.WriteLine(temp2 - temp3);
+            Console.WriteLine(temp1 + 2);
+            Console.WriteLine(temp2 + temp3);
             Console.WriteLine(temp1 * 3);
             Console.WriteLine(temp4 / 6.5m);
 
-            Console.WriteLine(Temperature.ConvertToC(temp4));
-            Console.WriteLine(Temperature.ConvertToF(temp1));
-            Console.WriteLine(Temperature.ConvertToC(temp1));
+            Console.WriteLine(Temperature.ConvertToCelsius(temp4));
+            Console.WriteLine(Temperature.ConvertToFahrenheit(temp1));
+            Console.WriteLine(Temperature.ConvertToCelsius(temp1));
 
-            Temperature temp5 = temp1;
-            Temperature temp6 = temp4;
+            var temp5 = temp1;
+            var temp6 = temp4;
             Console.WriteLine(temp1 > temp2);
             Console.WriteLine(temp2 < temp1);
             Console.WriteLine(temp1 == temp5);
             Console.WriteLine(temp1 == temp3);
             Console.WriteLine(temp1 <= temp4);
-            Console.WriteLine(temp1 == Temperature.ConvertToF(temp1));
+            Console.WriteLine(temp1 == Temperature.ConvertToFahrenheit(temp1));
             Console.WriteLine(temp4 == temp6);
             Console.WriteLine(temp1.GetHashCode() == temp5.GetHashCode());
-
 
             Console.ReadLine();
         }
--- CSharpBasics/TaskModule6/Temperature.cs
+++ CSharpBasics/TaskModule6/Temperature.cs
@@ -10,52 +10,32 @@ namespace TaskModule6
     /// This class allows you to work with the temperature in celsius or fahrenheit degrees. Celsius degrees are used by default. 
     /// Supports conversion from celsius to fahrenheit and back and basic operators.
     /// </summary>
-    class Temperature
+    public class Temperature
     {
         private const decimal CEL_TO_FAR = 9m / 5m;
-        private const decimal FAR_TO_CEL = 5m / 9m;
+        private const decimal FAR_TO_CEL = 1 / CEL_TO_FAR;
         private const decimal SHIFT = 32m;
-        private const decimal EXP = 1e-6m;
+        private const decimal EPS = 1e-6m;
 
-        decimal Degree { get; set; }
-        bool IsCelsius { get; set; }
-
-        public Temperature(decimal degree, bool isCelsius)
+        public decimal Degree { get; }
+        public bool IsCelsius { get; }
+        /// <summary>
+        /// Constructor for temperature class
+        /// </summary>
+        /// <param name="degree">Degrees</param>
+        /// <param name="isCelsius">true if Celsius, false if Fahrenheit</param>
+        public Temperature(decimal degree, bool isCelsius = true)
         {
-            this.Degree = degree;
-            this.IsCelsius = isCelsius;
+            Degree = degree;
+            IsCelsius = isCelsius;
         }
-
-        public Temperature(int degree, bool isCelsius)
-        {
-            this.Degree = degree;
-            this.IsCelsius = isCelsius;
-        }
-
-        public Temperature(double degree, bool isCelsius) : this((decimal)degree, isCelsius) { }
-
-        public Temperature(decimal degree) : this(degree, isCelsius: true) { }
-
-        public Temperature(double degree) : this((decimal)degree, isCelsius: true) { }
-
-        public Temperature(int degree) : this(degree, isCelsius: true) { }
 
         public static Temperature operator +(Temperature temp1, Temperature temp2)
         {
-            if (!temp1.IsCelsius && !temp2.IsCelsius)
+            if (temp1.IsCelsius == temp2.IsCelsius)
                 return new Temperature(temp1.Degree + temp2.Degree, isCelsius: false);
             else
-                return new Temperature(ConvertToC(temp1).Degree + ConvertToC(temp2).Degree, isCelsius: true);
-        }
-
-        public static Temperature operator +(Temperature temp, int change)
-        {
-            return new Temperature(temp.Degree + change, isCelsius: temp.IsCelsius);
-        }
-
-        public static Temperature operator +(int change, Temperature temp)
-        {
-            return new Temperature(temp.Degree + change, isCelsius: temp.IsCelsius);
+                return new Temperature(ConvertToCelsius(temp1).Degree + ConvertToCelsius(temp2).Degree, isCelsius: true);
         }
 
         public static Temperature operator +(Temperature temp, decimal change)
@@ -70,20 +50,10 @@ namespace TaskModule6
 
         public static Temperature operator -(Temperature temp1, Temperature temp2)
         {
-            if (!temp1.IsCelsius && !temp2.IsCelsius)
+            if (temp1.IsCelsius == temp2.IsCelsius)
                 return new Temperature(temp1.Degree - temp2.Degree, isCelsius: false);
             else
-                return new Temperature(ConvertToC(temp1).Degree - ConvertToC(temp2).Degree, isCelsius: true);
-        }
-
-        public static Temperature operator -(Temperature temp, int change)
-        {
-            return new Temperature(temp.Degree - change, isCelsius: temp.IsCelsius);
-        }
-
-        public static Temperature operator -(int change, Temperature temp)
-        {
-            return new Temperature(change - temp.Degree, isCelsius: temp.IsCelsius);
+                return new Temperature(ConvertToCelsius(temp1).Degree - ConvertToCelsius(temp2).Degree, isCelsius: true);
         }
 
         public static Temperature operator -(Temperature temp, decimal change)
@@ -98,20 +68,10 @@ namespace TaskModule6
 
         public static Temperature operator *(Temperature temp1, Temperature temp2)
         {
-            if (!temp1.IsCelsius && !temp2.IsCelsius)
+            if (temp1.IsCelsius == temp2.IsCelsius)
                 return new Temperature(temp1.Degree * temp2.Degree, isCelsius: false);
             else
-                return new Temperature(ConvertToC(temp1).Degree * ConvertToC(temp2).Degree, isCelsius: true);
-        }
-
-        public static Temperature operator *(Temperature temp, int change)
-        {
-            return new Temperature(temp.Degree * change, isCelsius: temp.IsCelsius);
-        }
-
-        public static Temperature operator *(int change, Temperature temp)
-        {
-            return new Temperature(temp.Degree * change, isCelsius: temp.IsCelsius);
+                return new Temperature(ConvertToCelsius(temp1).Degree * ConvertToCelsius(temp2).Degree, isCelsius: true);
         }
 
         public static Temperature operator *(Temperature temp, decimal change)
@@ -126,20 +86,10 @@ namespace TaskModule6
 
         public static Temperature operator /(Temperature temp1, Temperature temp2)
         {
-            if (!temp1.IsCelsius && !temp2.IsCelsius)
+            if (temp1.IsCelsius == temp2.IsCelsius)
                 return new Temperature(temp1.Degree / temp2.Degree, isCelsius: false);
             else
-                return new Temperature(ConvertToC(temp1).Degree / ConvertToC(temp2).Degree, isCelsius: true);
-        }
-
-        public static Temperature operator /(Temperature temp, int change)
-        {
-            return new Temperature(temp.Degree / change, isCelsius: temp.IsCelsius);
-        }
-
-        public static Temperature operator /(int change, Temperature temp)
-        {
-            return new Temperature(change / temp.Degree, isCelsius: temp.IsCelsius);
+                return new Temperature(ConvertToCelsius(temp1).Degree / ConvertToCelsius(temp2).Degree, isCelsius: true);
         }
 
         public static Temperature operator /(Temperature temp, decimal change)
@@ -157,7 +107,7 @@ namespace TaskModule6
         /// </summary>
         /// <param name="temp">temperature</param>
         /// <returns>temperature in fahrenheit</returns>
-        public static Temperature ConvertToF(Temperature temp)
+        public static Temperature ConvertToFahrenheit(Temperature temp)
         {
             return temp.IsCelsius ? new Temperature(CEL_TO_FAR * temp.Degree + SHIFT, isCelsius: false) : temp;
         }
@@ -167,27 +117,29 @@ namespace TaskModule6
         /// </summary>
         /// <param name="temp">temperature</param>
         /// <returns>temperature in celsius</returns>
-        public static Temperature ConvertToC(Temperature temp)
+        public static Temperature ConvertToCelsius(Temperature temp)
         {
             return temp.IsCelsius ? temp : new Temperature(FAR_TO_CEL * (temp.Degree - SHIFT), isCelsius: true);
         }
-
+        /// <inheritdoc/>
         public override string ToString()
         {
-            string type = this.IsCelsius ? "Celsius" : "Fahrenheit";
-            return string.Format("{0:N1} degrees {1}", this.Degree, type);
+            string type = IsCelsius ? "Celsius" : "Fahrenheit";
+            return $"{Degree:N1} degrees {type}";
         }
 
         public override bool Equals(object obj)
         {
-            if (obj is Temperature)
-                return Math.Abs(ConvertToC((Temperature)obj).Degree - ConvertToC(this).Degree) <= EXP;
+            if (obj is Temperature temp)
+            {
+                return Math.Abs(ConvertToCelsius(temp).Degree - ConvertToCelsius(this).Degree) <= EPS;
+            }
             return false;
         }
 
         public override int GetHashCode()
         {
-            return this.ToString().GetHashCode();
+            return ConvertToCelsius(this).Degree.GetHashCode();
         }
 
         public static bool operator ==(Temperature temp1, Temperature temp2)
@@ -202,12 +154,12 @@ namespace TaskModule6
 
         public static bool operator >(Temperature temp1, Temperature temp2)
         {
-            return ConvertToC(temp1).Degree - ConvertToC(temp2).Degree > EXP;
+            return ConvertToCelsius(temp1).Degree - ConvertToCelsius(temp2).Degree > EPS;
         }
 
         public static bool operator <(Temperature temp1, Temperature temp2)
         {
-            return ConvertToC(temp2).Degree - ConvertToC(temp1).Degree > EXP;
+            return ConvertToCelsius(temp2).Degree - ConvertToCelsius(temp1).Degree > EPS;
         }
 
         public static bool operator >=(Temperature temp1, Temperature temp2)
