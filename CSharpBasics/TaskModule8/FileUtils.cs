e8c71ba3e0c1d913d9402ac3e66a4b977afa17e9
Alexandra Klevtsova <alexandra.klevtsova@dataart.com>
2018-07-06 14:29:53 +0300

    Added project for third task

---
+++ CSharpBasics/TaskModule8/FileUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskModule8
+{
+    public enum FunctionType { Rename, RenameAll, Delete, DeleteAll }
+    public class FileUtils
+    {
+        public static void Rename(string filePath, string newName) { }
+
+        public static void RenameAll(string folderPath, string nameTemplate, bool recursive = false) { }
+
+        public static void Delete(string path, bool recursive = false) { }
+    }
+}
---
+++ CSharpBasics/TaskModule8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskModule8
+{
+    class Program
+    {
+        public enum FunctionType { Rename, RenameAll, Delete }
+        static void Main(string[] args)
+        {
+            bool isRecursive = false;
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine
+                    ("Must be args: \n" +
+                    "For Rename: Rename filePath newName\n" +
+                    "For RenameAll (added number to template): RenameAll folderPath nameTemplate [recurs]\n" +
+                    "For Delete: Delete path [recurs]\n");
+                return;
+            } 
+            
+            switch (args[0])
+            {
+                case "Rename":
+                    FileUtils.Rename(args[1], args[2]);
+                    break;
+                case "RenameAll":
+                    if (args.Count() > 3)
+                    {
+                        isRecursive = args[3] == "recurs";
+                    }
+                    FileUtils.RenameAll(args[1], args[2], isRecursive);
+                    break;
+                case "Delete":
+                    if (args.Count() > 2)
+                    {
+                        isRecursive = args[2] == "recurs";
+                    }
+                    FileUtils.Delete(args[1], isRecursive);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
