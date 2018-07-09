f35145d2763c139336cc7a7371f77e4818ddc587
Alexandra Klevtsova <alexandra.klevtsova@dataart.com>
2018-07-09 17:49:22 +0300

    Added project for final task. Added search utils and first version of menu

---
+++ CSharpBasics/FinalTask/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mask = string.Empty;
+            string pathFrom = string.Empty;
+            bool recursive = false;
+            string savepath = string.Empty;
+            //bool stopped = false;
+            bool overwrite = false;
+            try
+            {
+                Console.WriteLine("Please specify name or mask to search for:");
+                mask = Console.ReadLine();
+                Console.WriteLine("Please specify path to start from:");
+                pathFrom = Console.ReadLine();
+                Console.WriteLine("Should search be recursive? y - yes");
+                recursive = Console.ReadLine().ToLower() == "y";
+                do
+                {
+                    Console.WriteLine("Please specify filepath where search results must be saved (if empty results would be printed to console):");
+                    savepath = Console.ReadLine();
+                    if (File.Exists(savepath))
+                    {
+                        Console.WriteLine(savepath + " has already exists. Do you want to overwrite it? y - yes");
+                        overwrite = Console.ReadLine().ToLower() == "y";
+                    }
+                }
+                while (File.Exists(savepath) && !overwrite);
+
+                Console.WriteLine("Search in progress!");
+                if (savepath == string.Empty)
+                {
+                    var files = SearchUtils.Search(mask, pathFrom, recursive);
+                    foreach (var file in files)
+                    {
+                        Console.WriteLine(file);
+                    }
+                }
+                else
+                {
+                    SearchUtils.SearchAndSave(mask, pathFrom, savepath, recursive);
+                }
+                Console.WriteLine("Search is done!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpBasics/FinalTask/SearchUtils.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask
+{
+    /// <summary>
+    /// Class of utils for search in directories
+    /// </summary>
+    public class SearchUtils
+    {
+        /// <summary>
+        /// Searches for files that match the mask and saves them to file
+        /// </summary>
+        /// <param name="mask">Mask for files or filename</param>
+        /// <param name="startPath">Start directory</param>
+        /// <param name="savePath">Path to savefile</param>
+        /// <param name="recursive">True if search should be recursive</param>
+        public static void SearchAndSave(string mask, string startPath, string savePath, bool recursive = false)
+        {
+            using (StreamWriter saveFile = new StreamWriter(savePath))
+            {
+                SearchAndSaveRec(mask, startPath, saveFile, recursive);
+            }
+        }
+
+        /// <summary>
+        /// Searches for files that match the mask and returns IEnumerable collection of filenames
+        /// </summary>
+        /// <param name="mask">>Mask for files or filename</param>
+        /// <param name="startPath">Start directory</param>
+        /// <param name="recursive">True if search should be recursive</param>
+        /// <returns></returns>
+        public static IEnumerable<string> Search(string mask, string startPath, bool recursive = false)
+        {
+            var files = Directory.EnumerateFiles(startPath, mask);
+            if (recursive)
+            {
+                var directories = Directory.GetDirectories(startPath);
+                foreach (var dir in directories)
+                {
+                    files = files.Concat(Search(mask, dir, recursive));
+                }
+            }
+            return files;
+        }
+
+        private static void SearchAndSaveRec(string mask, string startPath, StreamWriter save, bool recursive = false)
+        {
+            var filesToSave = Directory.EnumerateFiles(startPath, mask);
+            foreach (var file in filesToSave)
+            {
+                save.WriteLine(file);
+            }
+            if (recursive)
+            {
+                var directories = Directory.GetDirectories(startPath);
+                foreach (var dir in directories)
+                {
+                    SearchAndSaveRec(mask, dir, save, recursive);
+                }
+            }
+        }
+    }
+}
