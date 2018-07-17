c636a31a59582b2f089a334145cbdfe543a17a28
Alexandra Klevtsova <alexandra.klevtsova@dataart.com>
2018-07-17 18:59:12 +0300

    Added Display format for dates

--- IncomeAndExpenses/IncomeAndExpenses.DataAccessInterface/Expense.cs
+++ IncomeAndExpenses/IncomeAndExpenses.DataAccessInterface/Expense.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace IncomeAndExpenses.DataAccessInterface
 {
     public class Expense : Entity<int>
     {
         public decimal Amount { get; set; }
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:MM/dd/yy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
         public string Comment { get; set; }
+
         public int ExpenseTypeId { get; set; }
         public virtual ExpenseType ExpenseType { get; set; }
     }
