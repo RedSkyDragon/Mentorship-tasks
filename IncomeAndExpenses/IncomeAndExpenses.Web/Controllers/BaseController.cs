df700c2271a8424c553dff13a2c6634bf52b5c68
Alexandra Klevtsova <alexandra.klevtsova@dataart.com>
2018-07-20 15:32:11 +0300

    Changet way to get userId

--- IncomeAndExpenses/IncomeAndExpenses.Web/Controllers/BaseController.cs
+++ IncomeAndExpenses/IncomeAndExpenses.Web/Controllers/BaseController.cs
@@ -1,18 +1,14 @@
 ﻿using IncomeAndExpenses.DataAccessImplement;
 using IncomeAndExpenses.DataAccessInterface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace IncomeAndExpenses.Web.Controllers
 {
     public class BaseController : Controller
     {
         protected IUnitOfWork _unitOfWork;
-        protected string UserId { get { return (HttpContext?.User.Identity as ClaimsIdentity)?.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).FirstOrDefault()?.Value; } }
+        protected string UserId { get { return User.Identity.GetUserId(); } }
 
         public BaseController(IUnitOfWork unitOfWork)
         {
