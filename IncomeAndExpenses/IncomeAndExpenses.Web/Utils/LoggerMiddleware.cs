c22bf4a889fdd2cd41b70534a3d0ae9d7afc6baf
Alexandra Klevtsova <alexandra.klevtsova@dataart.com>
2018-08-01 15:13:03 +0300

    Added logger middleware

---
+++ IncomeAndExpenses/IncomeAndExpenses.Web/Utils/LoggerMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace IncomeAndExpenses.Web.Utils
+{
+    public class LoggerMiddleware: OwinMiddleware
+    {
+        public LoggerMiddleware(OwinMiddleware next) : base(next) { }
+
+        public async override Task Invoke(IOwinContext context)
+        {
+            var logger = log4net.LogManager.GetLogger(GetType());
+            logger.Info("Start");
+            await Next.Invoke(context);
+            string message = Environment.NewLine + 
+                "URI: " + context.Request.Uri + Environment.NewLine +
+                "Method: " + context.Request.Method + Environment.NewLine +
+                "Status Code: " + context.Response.StatusCode; 
+            logger.Info(message);
+            logger.Info("Finish");
+        }
+    }
+}
