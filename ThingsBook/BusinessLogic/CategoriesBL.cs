f09c89eb396c1f5138dd92538eb4d4040ab312f5
Alexandra Klevtsova <alexandra.klevtsova@dataart.com>
2018-08-13 15:39:06 +0300

    First try on BL

---
+++ ThingsBook/BusinessLogic/CategoriesBL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThingsBook.DataAccessInterface;
+using MongoDB.Driver;
+using DataAccessImplement;
+
+namespace BusinessLogic
+{
+    public class CategoriesBL : ICategoriesBL
+    {
+        private ThingsBookContext _db;
+
+        public CategoriesBL(ThingsBookContext db)
+        {
+            _db = db;
+        }
+
+        public void CreateCategory(Guid userId, Category category)
+        {
+            var filter = Builders<User>.Filter.Where(u => u.Id == userId);
+            var update = Builders<User>.Update.Push(u => u.Categories, category);
+            _db.Users.FindOneAndUpdate(filter, update);
+        }
+
+        public void DeleteCategory(Guid userId, Guid id)
+        {
+            var filter = Builders<User>.Filter.Where(u => u.Id == userId);
+            var catFilter = Builders<Category>.Filter.Where(c => c.Id == id);
+            var update = Builders<User>.Update.PullFilter(u => u.Categories, catFilter);
+            _db.Users.FindOneAndUpdate(filter, update);
+        }
+
+        public IEnumerable<Category> GetCategories(Guid userId)
+        {
+            var filter = Builders<User>.Filter.Where(u => u.Id == userId);
+            return _db.Users.Find(filter).FirstOrDefault().Categories;
+        }
+
+        public Category GetCategory(Guid userId, Guid id)
+        {
+            var filter = Builders<User>.Filter.Where(u => u.Id == userId); ;
+            return _db.Users.Find(filter).FirstOrDefault().Categories.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public void UpdateCategory(Guid userId, Category category)
+        {
+            var filter = Builders<User>.Filter.Where(u => u.Id == userId && u.Categories.Any(cat => cat.Id == category.Id));
+            var update = Builders<User>.Update.Set(u => u.Categories.ElementAt(-1).Name, category.Name)
+                .Set(u => u.Categories.ElementAt(-1).About, category.About);
+            _db.Users.FindOneAndUpdate(filter, update);
+        }
+    }
+}
---
+++ ThingsBook/BusinessLogic/ICategoriesBL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThingsBook.DataAccessInterface;
+
+namespace BusinessLogic
+{
+    public interface ICategoriesBL
+    {
+        IEnumerable<Category> GetCategories(Guid userId);
+
+        Category GetCategory(Guid userId, Guid id);
+
+        void UpdateCategory(Guid userId, Category category);
+
+        void DeleteCategory(Guid userId, Guid id);
+
+        void CreateCategory(Guid userId, Category category);
+
+    }
+}
