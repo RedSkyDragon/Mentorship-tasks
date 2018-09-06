0f045bfaf726ee56509cab39f2363612174fa789
Alexandra Klevtsova <alexandra.klevtsova@dataart.com>
2018-09-06 20:09:35 +0300

    Renamed project

--- ThingsBookWebApp/e2e/src/app.e2e-spec.ts
+++ ThingsBookWebApp/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ThingsBookWebClient!');
+    expect(page.getParagraphText()).toEqual('Welcome to ThingsBookWebApp!');
   });
 });
